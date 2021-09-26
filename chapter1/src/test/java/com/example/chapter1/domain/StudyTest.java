6afa8e6e68b2526a43dadafaf3d717d8ca7eec26
seokrae <kslbsh@gmail.com>
2021-09-26 15:00:14 +0900

    feat: Junit - 태그 별 테스트

--- chapter1/src/test/java/com/example/chapter1/domain/StudyTest.java
+++ chapter1/src/test/java/com/example/chapter1/domain/StudyTest.java
@@ -1,11 +1,11 @@
 package com.example.chapter1.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 class StudyTest {
@@ -14,7 +14,7 @@ class StudyTest {
     @Test
     @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_11})
     void testCase1() {
-        Study study = new Study(100);
+        Study study = new Study(101);
         assertThat(study.getLimitCount()).isPositive();
     }
 
@@ -40,6 +40,24 @@ class StudyTest {
     @Test
     @EnabledIfEnvironmentVariable(named = "LOGNAME", matches = "seok")
     void testCase4() {
+        Study study = new Study(100);
+        assertThat(study.getLimitCount()).isPositive();
+    }
+
+    @DisplayName("빠른 테스트")
+    @Test
+    @Tag("fast")
+        // 로컬 테스트 (JUnit edit Configurations 에서 Tag 별로 실행 가능)
+    void testCase5() {
+        Study study = new Study(100);
+        assertThat(study.getLimitCount()).isPositive();
+    }
+
+    @DisplayName("느린 테스트")
+    @Test
+    @Tag("slow")
+        // CI  테스트
+    void testCase6() {
         Study study = new Study(100);
         assertThat(study.getLimitCount()).isPositive();
     }
