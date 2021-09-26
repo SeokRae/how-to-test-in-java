15d77b7bc72a4ac31e488121b483a676c35a0607
seokrae <kslbsh@gmail.com>
2021-09-26 15:16:12 +0900

    feat: Junit - 테스트 반복하기 1

--- chapter1/src/test/java/com/example/chapter1/domain/StudyTest.java
+++ chapter1/src/test/java/com/example/chapter1/domain/StudyTest.java
@@ -1,9 +1,10 @@
 package com.example.chapter1.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import com.example.chapter1.FastTest;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
@@ -46,8 +47,8 @@ class StudyTest {
 
     @DisplayName("빠른 테스트")
     @Test
+    // 로컬 테스트 (JUnit edit Configurations 에서 Tag 별로 실행 가능)
     @Tag("fast")
-        // 로컬 테스트 (JUnit edit Configurations 에서 Tag 별로 실행 가능)
     void testCase5() {
         Study study = new Study(100);
         assertThat(study.getLimitCount()).isPositive();
@@ -60,5 +61,26 @@ class StudyTest {
     void testCase6() {
         Study study = new Study(100);
         assertThat(study.getLimitCount()).isPositive();
+    }
+
+    @DisplayName("커스텀 태그 테스트")
+    @Test
+    @FastTest
+    void testCase7() {
+        Study study = new Study(100);
+        assertThat(study.getLimitCount()).isPositive();
+    }
+
+    @DisplayName("테스트 반복 테스트")
+    @RepeatedTest(value = 10, name = "repetition {displayName}, {currentRepetition}, {totalRepetitions}")
+    void testCase8(RepetitionInfo info) {
+        System.out.println("repetitionInfo = " + info.getCurrentRepetition() + "/" + info.getTotalRepetitions());
+    }
+
+    @DisplayName("ValueSource 테스트")
+    @ParameterizedTest(name = "{index} : {displayName} message:{0}")
+    @ValueSource(strings = {"진짜", "날씨가", "추워지고", "있습니다."})
+    void testCase9(String message) {
+        System.out.println("message = " + message);
     }
 }
